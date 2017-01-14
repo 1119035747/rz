42de9e02bc95026ed7e439cfc747ce29096c6fe2
1119035747 <850718880@qq.com>
2017-01-14 16:54:45 +0800

    Add files via upload

---
+++ fontMark.php
@@ -0,0 +1,13 @@
+<?php
+	$src="001.jpg";
+
+	$info = getimagesize($src);
+
+	$type = image_type_to_extension($info[2],false);
+
+	print_r($type);
+	
+	$fun = "imagecreatefrom($type)";
+
+	$image = $fun($src);
+?>
